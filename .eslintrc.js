07817415e847c82ade309089963b80e082115712
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-10 18:17:43 -0300

    added eslint, prettier and editorconfig to project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "class-methos-use-this": "off",
+    "no-param-reassign": "off",
+    "camel-case": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "prettier/prettier": "error",
+  },
+};

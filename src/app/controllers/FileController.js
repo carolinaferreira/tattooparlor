c3026f7deed4e2422c66730cc40df32cdf139017
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-14 10:50:01 -0300

    created files table at database, file controller and model

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,17 @@
+import File from '../models/File';
+
+/* eslint-disable class-methods-use-this */
+class FileController {
+  async store(req, res) {
+    const { originalname: name, filename: path } = req.file;
+
+    const file = await File.create({
+      name,
+      path,
+    });
+
+    return res.json(file);
+  }
+}
+
+export default new FileController();

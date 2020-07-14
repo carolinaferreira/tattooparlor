a13c69b6f596731b2516c3e193748c1cb30fdf91
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-14 11:43:38 -0300

    added route to list users that are providers

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,8 @@ routes.post('/sessions', SessionsController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

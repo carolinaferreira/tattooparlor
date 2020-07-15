bbc58d5f2587ce6722c7ca8a5034f83a306608fd
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-15 16:30:54 -0300

    added create appointment method

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,39 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+/* eslint-disable class-methods-use-this */
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create apponitments with providers' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();

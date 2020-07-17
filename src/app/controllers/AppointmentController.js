3ee5f0f4db534ef288c0a74fb905c228474783b5
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-17 19:28:33 -0300

    added schemas and mongodb

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,11 @@
+/* eslint-disable prettier/prettier */
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 /* eslint-disable class-methods-use-this */
 class AppointmentController {
@@ -77,6 +80,15 @@ class AppointmentController {
       date: hourStart,
     });
 
+    // Notify apppointment provider
+    const user = await User.findByPk(req.userId);
+
+    const formattedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", {locale: pt});
+
+    await Notification.create({
+      content: `Nova agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
+    });
     return res.json(appointment);
   }
 }

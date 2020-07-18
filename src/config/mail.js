eaaa04362bb83ada1620a515fb77d72b39c0aafd
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-18 20:50:58 -0300

    added nodemailer and configured an email to when appointment is canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ad4352d8943042',
+    pass: '6867a5453c75b7',
+  },
+  default: {
+    from: 'Tattoo Parlor Team <noreply@tattooparlor.com>',
+  },
+};
